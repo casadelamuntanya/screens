59ff9e26783aca37bd4d49c5cade0a0f6eb08ea7
markusand <mvilella@obsa.ad>
2020-06-15 18:05:09 +0200

    Add About page
    + Youtube video presentation

---
+++ src/locales/ca/about.js
@@ -0,0 +1,5 @@
+export default {
+	description: 'Estem creant un Laboratori de Muntanya. Un espai on experimentar la muntanya, però també on desenvolupar-hi nous projectes i implementar-los en un entron real.',
+
+	goal: "El nostre objectiu és involucrar tothom -des d'empreses tecnològiques a guies, des de centres acadèmics a artistes- per dissenyar, construir i provar idees sorprenents.",
+};
---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="grid">
+		<div class="row row--fit">
+			<youtube-video video-id="YAc0X-E4sXk" :subs="$i18n.locale" />
+		</div>
+		<div class="row">
+			<div class="col col--4">
+				<div class="box">
+					<h3>Casa de la Muntanya</h3>
+				</div>
+			</div>
+			<div class="col">
+				<div class="box">
+					<p>{{ $t('about.description') }}</p>
+					<p>{{ $t('about.goal') }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import YoutubeVideo from '@/components/YoutubeVideo.vue';
+
+export default {
+	name: 'about',
+	components: { YoutubeVideo },
+};
+</script>
