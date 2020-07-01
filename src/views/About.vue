f6b6ea1510a139eb608fd1f03c7b20576d73642b
markusand <mvilella@obsa.ad>
2020-07-01 15:04:18 +0200

    Add CdM floorplan
    + Add ImageMarkers component
    + Wrap view config into its own file

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="grid">
 		<div class="row row--fit">
-			<youtube-video video-id="YAc0X-E4sXk" :subs="$i18n.locale" />
+			<youtube-video :video-id="introYoutubeVideo" :subs="$i18n.locale" />
 		</div>
-		<div class="row">
+		<div class="row row--fit">
 			<div class="col col--4">
 				<div class="box">
 					<h3>Casa de la Muntanya</h3>
@@ -16,14 +16,32 @@
 				</div>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col col--fit col--center col--middle">
+				<image-markers :markers="markers" class="floorplan" >
+					<img svg-inline src="@/assets/vectors/floorplan.svg" />
+					<i slot="marker" slot-scope="{ marker }" :class="marker.icon" />
+					<div slot="popup" slot-scope="{ marker }" class="popup">
+						<h6>{{ $t(`about.floorplan.${marker.id}.name`) }}</h6>
+						<p>{{ $t(`about.floorplan.${marker.id}.description`)}}</p>
+					</div>
+				</image-markers>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import YoutubeVideo from '@/components/YoutubeVideo.vue';
+import ImageMarkers from '@/components/ImageMarkers.vue';
+
+import { introYoutubeVideo, markers } from '@/config/about';
 
 export default {
 	name: 'about',
-	components: { YoutubeVideo },
+	components: { YoutubeVideo, ImageMarkers },
+	data() {
+		return { introYoutubeVideo, markers };
+	},
 };
 </script>
